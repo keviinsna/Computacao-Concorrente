b7a2b70e64e0fe0cdd36c76de8bde8e23277cfa7
kevinsena01 <keevinsenna2001@gmail.com>
2021-11-20 20:31:21 -0300

    criando várias threads

--- Lab1/hello.c
+++ Lab1/hello.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <pthread.h>
 
+#define NTHREADS 10
+
 /* função que a thread irá executar */
 void * tarefa (void * arg){
-	printf("Olá, sou a thread nova!\n");
+	 /* Como estou passando o endereço, precisamos usar o * para usar o valor do ponteiro*/
+	int ident = * (int *) arg;
+	printf("Olá, sou a thread %d!\n", ident);
 	
         /* função que finaliza a thread e retorna seu valor*/	
 	pthread_exit(NULL);
@@ -11,22 +15,16 @@ void * tarefa (void * arg){
 
 /* função principal */
 int main(void){
-	pthread_t tid; /* identificador da thread no sistema*/
-	
-	/* Cria uma nova thread recebendo seus 4 parâmetros:
-	 * 1 - O identificador da thread que será usado pelo SO e por nós para nos referirmos à tal thread.
-	 * 2 - Atributos para a thread, tais como: tamanho da pilha de execução, questões de prioridades etc.
-	 * 3 - função que será executada.
-	 * 4 - argumentos da função que será executada
-	*/	
-	if(pthread_create(&tid, NULL, tarefa, NULL))
-		printf("EROO -- pthread_create\n");
-	
-	printf("Olá, sou a thread principal!\n");
+	pthread_t tid[NTHREADS]; /* identificadores de cada thread no sistema*/
+	int ident[NTHREADS];   /* identificador local da thread*/	
+	int i;
 
-	/* Desvincula o término da mais do término do programa. 
-	 * Caso a main termine antes, o programa continua ativo até que todas as threads terminem.
-	 */
+	for(i = 0; i < NTHREADS; i++){	
+		ident[i] = i + 1; /* 1 a 10  */
+		if(pthread_create(&tid[i], NULL, tarefa, (void *)&ident[i]))
+			printf("EROO -- pthread_create\n");
+	}
+	printf("Olá, sou a thread principal!\n");
 	pthread_exit(NULL);	
 	return 0;
 }
