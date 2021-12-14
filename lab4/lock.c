dd0b3db3af27c954b3ad518878b6dbd76f5e9e63
kevinsena01 <keevinsenna2001@gmail.com>
2021-12-14 18:55:34 -0300

    Sincronização com lock

--- lab4/lock.c
+++ lab4/lock.c
@@ -1,4 +1,4 @@
-/*Condição de corrida entre as 2 threads - pt2*/
+/*Tratando a condição de corrida com lock*/
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,14 +7,22 @@
 #define NTHREADS 2
 
 int s = 0; // Variável compartilhada
+pthread_mutex_t lock; // Variável para sincronização de exclusão mútua
 
 // Função execu11otada pelas threads
 void *tarefa(void *arg){
 	long int id = (long int) arg;
 	printf("Thread %ld está executando...\n", id);
 
-	for(int i = 0; i < 10000; i++)
+	for(int i = 0; i < 10000; i++){
+		// Entrada de seção crítica
+		pthread_mutex_lock(&lock);
+	
 		s++; // Variável compartilhada
+	
+		// Saída de seção crítica
+		pthread_mutex_unlock(&lock);
+	}
 	printf("Thread %ld terminou.\n", id);
 	pthread_exit(NULL);
 }
@@ -22,6 +30,9 @@ void *tarefa(void *arg){
 // Fluxo principal
 int main(int argc, char * argv[]){
 	pthread_t tid[NTHREADS];
+	
+	// Inicializa a variável de exclusão mútua
+	pthread_mutex_init(&lock, NULL);
 
 	// Cria as threads
 	for(long int i = 0; i < NTHREADS; i++){
@@ -39,5 +50,8 @@ int main(int argc, char * argv[]){
 		}
 	}
 	printf("Valor de s: %d\n", s);
+	
+	// Libera o lock
+	pthread_mutex_destroy(&lock);
 	return 0;
 }
